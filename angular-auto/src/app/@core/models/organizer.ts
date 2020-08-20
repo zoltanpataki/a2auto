f6706405504af326c2c65e7c1d4f738af84a2d3c
zoltanpataki <zolee7777@gmail.com>
2020-08-21 01:05:44 +0200

    organizer model added

---
+++ angular-auto/src/app/@core/models/organizer.ts
@@ -0,0 +1,12 @@
+export class Organizer {
+  value: string;
+  viewValue: string;
+  direction: string;
+
+  constructor(value: string, viewValue:string, direction: string) {
+    this.value = value;
+    this.viewValue = viewValue;
+    this.direction = direction;
+  }
+}
+
