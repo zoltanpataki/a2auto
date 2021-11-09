6e06f9d8d988208af5bec84de8bf68eb7938f999
zoltanpataki <zolee7777@gmail.com>
2021-11-09 22:18:31 +0100

    ordering redesigned

--- angular-auto/src/app/@core/models/organizer.ts
+++ angular-auto/src/app/@core/models/organizer.ts
@@ -2,11 +2,15 @@ export class Organizer {
   value: string;
   viewValue: string;
   direction: Direction;
+  icon: string;
+  iconColor: string;
 
-  constructor(value: string, viewValue:string, direction: Direction) {
+  constructor(value: string, viewValue:string, direction: Direction, icon: string, iconColor: string) {
     this.value = value;
     this.viewValue = viewValue;
     this.direction = direction;
+    this.icon = icon;
+    this.iconColor = iconColor;
   }
 }
 
